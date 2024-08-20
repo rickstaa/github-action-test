c9e5bac01eb19e0d625f7cfca5e6c22ded90a408
Rick Staa <rick.staa@outlook.com>
2024-08-20 10:14:05 +0200

    ci: put tag logic in script

---
+++ ci_env.sh
@@ -0,0 +1,4 @@
+# Retrieve latest release tag
+git fetch --tags
+latest_tag=$(git tag -l "v*" | grep -E '^v[0-9]+\.[0-9]+\.[0-9]+$' | sort -V | tail -n 1)
+echo "latest_tag=$latest_tag" >>"$GITHUB_OUTPUT"
